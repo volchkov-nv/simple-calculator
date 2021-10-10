1018f4acfca65d5f4ca36565c8f97309f58ba971
Volchkov <nvolchkov@baccasoft.ru>
2021-10-10 19:16:32 +0400

    Coroutine fix

--- app/src/main/java/com/example/simplecalculator/domain/repos/SimpleCalculatorRepository.kt
+++ app/src/main/java/com/example/simplecalculator/domain/repos/SimpleCalculatorRepository.kt
@@ -1,13 +1,14 @@
 package com.example.simplecalculator.domain.repos
 
 import com.example.simplecalculator.domain.models.HistoryModel
+import kotlinx.coroutines.CoroutineScope
 
 interface SimpleCalculatorRepository {
 
-    fun getAllHistoryData(onSuccess: (List<HistoryModel>) -> Unit)
+    fun getAllHistoryData(onSuccess: (List<HistoryModel>) -> Unit, scope: CoroutineScope)
 
-    fun setNewHistoryData(model: HistoryModel, onSuccess: (Long) -> Unit, onError: () -> Unit)
+    fun setNewHistoryData(model: HistoryModel, onSuccess: (Long) -> Unit, onError: () -> Unit, scope: CoroutineScope)
 
-    fun clearAllHistory()
+    fun clearAllHistory(scope: CoroutineScope)
 
 }
